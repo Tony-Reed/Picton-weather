5fe687908d43e9b80861b7510ac6d699e73bc70b
Tony-Reed <anthony.reed@gmail.com>
2017-04-26 17:53:58 -0400

    Update test_darksky.php

--- test_darksky.php
+++ test_darksky.php
@@ -2,7 +2,8 @@
 
 // <?php
 //  DarkSky API
-//  position at 23 Ferguson Street, Picton, ON CA.
+//  GPS polition at 23 Ferguson Street, Picton, ON CA
+//  (my desk)
 
 $lat=44.004401;
 $lon=-77.145244;
@@ -10,8 +11,10 @@ $key=getenv('DARK_SKY_KEY');
 $opt="?units=si";
 $url="https://api.darksky.net/forecast/";
 
+// build a URL 
 $sky=$url . $key . "/" . $lat . "," . $lon . $opt;  
 
+// debugging
 //echo $sky;
 //return true;
 
@@ -22,8 +25,11 @@ $ch=curl_init();
 curl_setopt($ch, CURLOPT_URL, $sky);
 curl_setopt($ch, CURLOPT_HEADER, 0);
 
+// open  a file
 $fp = fopen("dsky.json", "w");
 curl_setopt($ch, CURLOPT_FILE, $fp);
+
+// grab the output
 curl_exec($ch);
 
 // return the transfer as a string 

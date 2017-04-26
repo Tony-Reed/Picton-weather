7ba057cf11e3cfbcfe96660f76829fea3a88a06f
Tony-Reed <anthony.reed@gmail.com>
2017-04-26 17:31:47 -0400

    Add files via upload

--- test_darksky.php
+++ test_darksky.php
@@ -1,11 +1,11 @@
 #! /usr/bin/env php
 
 // <?php
-
+//  DarkSky API
 //  position at 23 Ferguson Street, Picton, ON CA.
 
-$lat=44.00434;
-$lon=-77.14530;
+$lat=44.004401;
+$lon=-77.145244;
 $key=getenv('DARK_SKY_KEY');
 $opt="?units=si";
 $url="https://api.darksky.net/forecast/";
@@ -15,31 +15,29 @@ $sky=$url . $key . "/" . $lat . "," . $lon . $opt;
 //echo $sky;
 //return true;
 
-
 // create a curl resource 
 $ch=curl_init();
 
 // set url 
 curl_setopt($ch, CURLOPT_URL, $sky);
-curl_setopt($ch, CURLOPT_HEADER, false);
+curl_setopt($ch, CURLOPT_HEADER, 0);
+
+$fp = fopen("dsky.json", "w");
+curl_setopt($ch, CURLOPT_FILE, $fp);
+curl_exec($ch);
 
 // return the transfer as a string 
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
-$output = curl_exec($ch); 
+//curl_setopt($ch, CURLOPT_RETURNTRANSFER, 0); 
+
+//$output = curl_exec($ch); 
 
 // $output contains the output string 
 //var_dump($output);
 
-var_dump(json_decode($output, true));
+//var_dump(json_decode($output, true));
 
+fclose($fp);
 curl_close($ch);      
-
-//$fp = fopen("sky.txt", "w");
-
-//curl_setopt($ch, CURLOPT_FILE, $fp);
-//curl_setopt($ch, CURLOPT_HEADER, 0);
-//fclose($fp);
-
 ?>
 
 

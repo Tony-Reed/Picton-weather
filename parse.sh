4c2231b9964fb17dc88b4c2d695ec44361d7a7b2
Tony-Reed <anthony.reed@gmail.com>
2017-05-07 15:22:38 -0400

    this an that

--- parse.sh
+++ parse.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 
-php test_darksky.php
+#php test_darksky.php
+#jq '.currently.time' < dksky.json
 
-jq '.currently' < dksky.json
-jq '.daily .data[0] .time' < dksky.json
-jq '.alerts[] .title' <dksky.json
-jq '.alerts[] .regions[2]' <dksky.json
-jq '.alerts[] .description' <dksky.json
+cur=$(jq '.currently.time' < dksky.json)
+date --date=@$cur
+jq '.currently.summary' < dksky.json
+
+
+
+#jq '.daily .data[0] .time' < dksky.json
+
+
+
 

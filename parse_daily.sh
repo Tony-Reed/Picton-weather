483f632982537484d866e148492650d13ff46de7
Tony-Reed <anthony.reed@gmail.com>
2017-05-17 08:14:13 -0400

    formatting "daily" forecasts

--- parse_daily.sh
+++ parse_daily.sh
@@ -1,15 +1,19 @@
 #!/bin/sh
 
-sunDown=$(jq '.daily.data[0].sunsetTime' < dksky.json)
-sunUp=$(jq '.daily.data[0].sunriseTime' < dksky.json)
-curDay=$(jq '.daily.data[0].time' < dksky.json)
+tempMax=$(jq '.daily.data[0]|.temperatureMax' dksky.json)
+tempMin=$(jq ' .daily.data[0]|.temperatureMin' dksky.json)
+sunDown=$(jq '.daily.data[0].sunsetTime' dksky.json)
+sunUp=$(jq '.daily.data[0].sunriseTime' dksky.json)
+curDay=$(jq '.daily.data[0].time' dksky.json)
+
 date "+%A " --date=@$curDay
-jq '.daily.data[0].summary' < dksky.json
-printf "Today's high: "
-jq '.daily.data[0].temperatureMax' <dksky.json
-printf "Overnight: "
-jq '.daily.data[0].temperatureMin' <dksky.json
-printf  "Sunrise:  "
+jq '.daily.data[0].summary' dksky.json
+
+printf  "Today's high: %.0f\n" $tempMax
+printf "Overnight: %.0f\n" $tempMin
+
+printf "Sunrise: "
 date "+ %H:%M:%S" --date=@$sunUp
-printf "Sunset:   "
+printf "Sunset: "
 date "+ %H:%M:%S" --date=@$sunDown
+

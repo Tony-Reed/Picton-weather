9584a6f9a3ea5130ae31fa76dbc4cb6c04afa734
Tony-Reed <anthony.reed@gmail.com>
2017-05-19 10:56:26 -0400

    tidying up code

--- darksky.php
+++ darksky.php
@@ -1,54 +1,37 @@
 <?php
 
-  mb_internal_encoding("UTF-8");
+mb_internal_encoding("UTF-8");
 
-// lat & long point to 23 Ferguson St., Picton, Ontario, Canada
+// lat & long lead to 23 Ferguson St., Picton, Ontario, Canada
 $lat=44.0044;
 $lon=-77.1453;
+
 // Dark Sky API key somewhere safe
 $key=getenv('DARK_SKY_KEY');
-//$opt="?units=ca";
-$opt="?units=uk2";
+
 $url="https://api.darksky.net/forecast/";
 
-// build a customised url
-$sky=$url . $key . "/" . $lat . "," . $lon . $opt;  
+// we're not working with the minute-by-minute forecast
+// we don't need the weatherstation flags
+$exc="?exclude=minutely,flags";
+
+$opt="&units=uk2";
+
+// build a custom url
+$dkSky=$url . $key . "/" . $lat . "," . $lon . $exc .  $opt;  
+//echo  $dkSky;
+//return true;
+
 
 // download the forecast
-$sky=file_get_contents($sky);
-//$sky=utf8_encode($sky);
+$dkSky=file_get_contents($dkSky);
 
 // write it locally
-$fp=fopen("dksky.json", "w");
-fwrite($fp, $sky);
+$fp=fopen("dkSky.json", "w");
+fwrite($fp, $dkSky);
 // tidy up
 fclose($fp);
 
 
-
-
-
-
-/* 
-Another approach
-
-create a curl resource 
-$ch=curl_init();
-
-// set url 
-curl_setopt($ch, CURLOPT_URL, $sky);
-curl_setopt($ch, CURLOPT_HEADER, 0);
-
-// open output file for writing & write data to it
-$fp = fopen($out, "w");
-curl_setopt($ch, CURLOPT_FILE, $fp);
-curl_exec($ch);
-
-//$out=json_decode($out, true);
-
-//clean up
-fclose($fp);
-curl_close($ch);      
-*/
 
 

b7cfa637ac53060159f6e6189b27efdfc00c2d15
Tony-Reed <anthony.reed@gmail.com>
2017-05-22 18:32:29 -0400

    changes

--- parse_daily.sh
+++ parse_daily.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 php darksky.php
 
@@ -8,14 +8,15 @@ tempMax=$(jq '.daily.data[0]|.temperatureMax' dkSky.json)
 tempMin=$(jq '.daily.data[0]|.temperatureMin' dkSky.json)
 sunDown=$(jq '.daily.data[0].sunsetTime' dkSky.json)
 sunUp=$(jq '.daily.data[0].sunriseTime' dkSky.json)
-
 curDay=$(jq '.daily.data[0].time' dkSky.json)
+sum=$(jq '.daily.data[0].summary' dkSky.json)
 
 #jq  '.daily.summary' dkSky.json
 
 date "+%A " --date=@$curDay
-jq  '.daily.data[0].summary' dkSky.json
+#jq  '.daily.data[0].summary' dkSky.json
 
+printf "$sum\n"
 printf  "Today's high: %.0f\n" $tempMax
 printf "Overnight: %.0f\n" $tempMin
 

9a540ad0bb6312e2af9e9d31b2efe51847be1a05
Tony-Reed <anthony.reed@gmail.com>
2017-05-23 18:51:26 -0400

    tidying & comments

--- darksky.php
+++ darksky.php
@@ -2,26 +2,26 @@
 
 mb_internal_encoding("UTF-8");
 
-// lat & long lead to 23 Ferguson St., Picton, Ontario, Canada
+// lat & long lead for Ferguson St., Picton, Ontario, Canada
 $lat=44.0044;
 $lon=-77.1453;
 
 // Dark Sky API key somewhere safe
 $key=getenv('DARK_SKY_KEY');
-
 $url="https://api.darksky.net/forecast/";
 
-// we're not working with the minute-by-minute forecast
+// we're not (yet) working with the minute-by-minute forecast
 // we don't need the weatherstation flags
 $exc="?exclude=minutely,flags";
-
-$opt="&units=uk2";
+$opt="&units=uk2"; //windspeed in MPH
 
 // build a custom url
 $dkSky=$url . $key . "/" . $lat . "," . $lon . $exc .  $opt;  
-//echo  $dkSky;
-//return true;
-
+/*
+//debugging url
+echo  $dkSky;
+return true;
+*/
 
 // download the forecast
 $dkSky=file_get_contents($dkSky);

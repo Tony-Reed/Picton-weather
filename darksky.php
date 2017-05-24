cc904b6593992ac89641f896745c9c0bbeeccdb1
Tony-Reed <anthony.reed@gmail.com>
2017-05-24 13:14:30 -0400

    Add files via upload

--- darksky.php
+++ darksky.php
@@ -2,26 +2,25 @@
 
 mb_internal_encoding("UTF-8");
 
-// lat & long lead for Ferguson St., Picton, Ontario, Canada
+// lat & long lead to 23 Ferguson St., Picton, Ontario, Canada
 $lat=44.0044;
 $lon=-77.1453;
 
 // Dark Sky API key somewhere safe
 $key=getenv('DARK_SKY_KEY');
+
 $url="https://api.darksky.net/forecast/";
 
-// we're not (yet) working with the minute-by-minute forecast
+// we're not working with the minute-by-minute forecast
 // we don't need the weatherstation flags
 $exc="?exclude=minutely,flags";
-$opt="&units=uk2"; //windspeed in MPH
+
+$opt="&units=uk2";
 
 // build a custom url
 $dkSky=$url . $key . "/" . $lat . "," . $lon . $exc .  $opt;  
-/*
-//debugging url
-echo  $dkSky;
-return true;
-*/
+//echo  $dkSky;
+//return true;
 
 // download the forecast
 $dkSky=file_get_contents($dkSky);
---
+++ index.php
@@ -0,0 +1,42 @@
+<?php
+header("Content-type: text/html; charset=utf-8");
+header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
+header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+header("Expires: Fri, 22 Nov 1963 18:30:00 GMT\n"); // Date in the past
+header("Content-Transfer-Encoding: binary");
+header("Creation-Date: Wed, 24 May 2017 12:00:00 GMT/n");
+?>
+
+<!DOCTYPE html>
+
+<!--   
+     I hope you're having a nice day
+     Website by Tony Reed
+-->
+
+<html lang="en">
+
+<head> 
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+
+<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Weather  Web Site</title>
+<link href="main.css" rel="stylesheet" />
+<meta name="title" content="Tony Picton Weather" />
+<link rel="shortcut icon" href="http://www.trljc.com/favicon.ico" type="image/x-icon" /> 
+</head>
+
+<body>
+
+<?php 
+@include "times.php";
+//echo "<footer id=\"date\" title=\"Today's Date\">" . $today. "</footer>";?>
+
+
+</body>
+</html>
+

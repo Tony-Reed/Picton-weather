6ae3e562fe335dc67d4fd8ded7e0cb39fba03a8f
Tony-Reed <anthony.reed@gmail.com>
2017-05-24 22:39:40 -0400

    add some comments

--- times.php
+++ times.php
@@ -1,15 +1,15 @@
 <?php
-
-mb_internal_encoding("UTF-8");
-
-
+//mb_internal_encoding("UTF-8");
 //echo date('l jS F Y H:i:s e', 1494116640);
 
+// get the DarkSky forecast
 @include "darksky.php";
 
+// print current conditions
 $output = shell_exec('./weather_now');
 printf($output);
 
+// print seven-day forecast
 $output1 = shell_exec('./seven_day_forecast');
 printf($output1);
 

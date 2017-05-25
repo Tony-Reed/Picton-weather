b344d2cc50fb70b7a1d9a49d5347a027ada4e6e2
Tony-Reed <anthony.reed@gmail.com>
2017-05-24 22:30:59 -0400

    tidy up the structure a bit

--- darksky.php
+++ darksky.php
@@ -5,30 +5,28 @@ mb_internal_encoding("UTF-8");
 // lat & long lead to 23 Ferguson St., Picton, Ontario, Canada
 $lat=44.0044;
 $lon=-77.1453;
-
 // Dark Sky API key somewhere safe
 $key=getenv('DARK_SKY_KEY');
-
+// Dark Sky magic
 $url="https://api.darksky.net/forecast/";
-
 // we're not working with the minute-by-minute forecast
 // we don't need the weatherstation flags
 $exc="?exclude=minutely,flags";
-
 $opt="&units=uk2";
 
-// build a custom url
+// put together a custom url
 $dkSky=$url . $key . "/" . $lat . "," . $lon . $exc .  $opt;  
+/*
+// debugging
 //echo  $dkSky;
 //return true;
-
+*/
 // download the forecast
 $dkSky=file_get_contents($dkSky);
 
-// write it locally
+// save it to work with
 $fp=fopen("dkSky.json", "w");
 fwrite($fp, $dkSky);
-// tidy up
 fclose($fp);
 
 

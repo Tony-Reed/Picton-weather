214dbb89684dee3be5e5b83f900a11929167de35
Tony-Reed <anthony.reed@gmail.com>
2017-06-02 16:55:10 -0400

    modified:   alerts 	modified:   dkSky.json 	modified:   index.php 	modified:   weather_now tidy tidy tidy

--- index.php
+++ index.php
@@ -23,32 +23,30 @@ if(!ob_start("ob_gzhandler")) ob_start();
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1" />
-
 <!-- The above 3 meta tags *must* come first in the head -->
 
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>Weather Web Site</title>
 <link href="main.css" rel="stylesheet" />
 <link rel="shortcut icon" href="http://www.trljc.com/favicon.ico" type="image/x-icon" /> 
-</head>
 
+</head>
 <body>
 <header> 
 <h1>The weather in Picton Ontario Canada, <br>powered by <a href="https://darksky.net/poweredby/"> DarkSky </a></h1>
 </header>
-<article>
 
+<article>
 <?php 
 // call in the cavalry      
 
      @include "times.php";
  
-//echo "<footer id=\"date\" title=\"Today's Date\">" . $today. "</footer>";
 while (@ob_end_flush());
 ?>
 
 </article>
-
+//echo "<footer id=\"date\" title=\"Today's Date\">" . $today. "</footer>";
 </body>
 </html>
 
